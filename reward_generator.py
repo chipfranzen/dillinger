79bc5ca465c8d75fa735e25a785b8d458cb706ce
Charles Franzen <charlesfranzen@Charless-Air.hsd1.ca.comcast.net>
2017-07-24 10:15:09 -0700

    add type hinting to reward_generator

--- reward_generator.py
+++ reward_generator.py
@@ -13,7 +13,11 @@ import matplotlib.pyplot as plt
 
 
 class RewardGenerator:
-    def __init__(self, min_price, max_price, n_price_points, seed=None):
+    def __init__(self,
+                 min_price: float,
+                 max_price: float,
+                 n_price_points: int,
+                 seed=None):
         self.max_price = max_price
         self.min_price = min_price
         self.n_price_points = n_price_points
@@ -25,8 +29,10 @@ class RewardGenerator:
             np.random.seed(seed)
 
     def create_spend_distributions(self, variance_level=1000):
-        '''this creates high variance gamma distributions for when customers
-        decide to spend'''
+        '''
+        this creates high variance gamma distributions for when customers
+        decide to spend
+        '''
         self.dists = []
         for exp, prob in zip(self.expected_value, self.p):
             theta = stats.gamma(variance_level).rvs()
@@ -46,12 +52,12 @@ class RewardGenerator:
         plt.xlabel('price')
         plt.legend()
 
-    def pull_arm(self, arm_index):
+    def pull_arm(self, arm_index: int) -> float:
         '''
         returns a pull from a multi-armed bandit
 
         inputs:
-        arm_index (int): index of the arm to be pulled
+        arm_index: index of the arm to be pulled
 
         output (float): observed reward
         '''
@@ -60,9 +66,11 @@ class RewardGenerator:
         else:
             return 0
 
-    def sample(self, n_draws):
-        # draws random samples from the reward generator
-        # by pulling arms uniformly
+    def sample(self, n_draws: int):
+        '''
+        draws random samples from the reward generator by pulling arms
+        uniformly
+        '''
         draws = np.zeros((n_draws, 2))
         for i in range(n_draws):
             price = np.random.randint(self.n_price_points)
@@ -71,7 +79,9 @@ class RewardGenerator:
         return draws
 
     def set_conversion_rates(self, x0=5, k=.7, max_conversion_rate=.1):
-        # create spend probabilities
+        '''
+        create spend probabilities
+        '''
         self.x0 = x0  # x-value of the midpoint
         self.k = k  # steepness of the curve
         self.max_conversion_rate = max_conversion_rate
